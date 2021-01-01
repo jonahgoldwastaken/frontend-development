225030e78a7775ed4ca8fe2a87fb262a27ce7e35
Jonah Meijers <jonahmeijers97@gmail.com>
2021-01-01 23:48:03 +0000

    create pricing page styling, remove BEM

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,47 @@
-window.addEventListener("DOMContentLoaded", onLoad);
+window.addEventListener('DOMContentLoaded', onLoad)
 
 function onLoad() {
   /**
    * Event Listeners
    */
   document
-    .querySelector("[data-open-menu]")
-    .addEventListener("click", toggleHeaderMenu);
+    .querySelector('[data-open-menu]')
+    .addEventListener('click', toggleHeaderMenu)
 
   /**
    * Create intersection observer
    */
   const observer = new IntersectionObserver(intersectionHandler, {
-    rootMargin: "-1px 0px 0px 0px",
+    rootMargin: '-1px 0px 0px 0px',
     threshold: [0, 1],
-  });
-  
+  })
+
   /**
    * Add class to and observe all intersectable elements
    */
   const elements = document.querySelectorAll('[data-intersect]')
-  for (element of elements) {
-     observer.observe(element);
+  for (const element of elements) {
+    observer.observe(element)
   }
   addClassToIntersectableElements(elements)
 }
 
 function addClassToIntersectableElements(elements) {
-  for (element of elements) {
+  for (const element of elements) {
     element.classList.add('intersectable')
   }
 }
 
 function toggleHeaderMenu() {
-  document.querySelector("header").classList.toggle("nav-open");
-  document.body.classList.toggle("overlay-open");
+  document.querySelector('header').classList.toggle('nav-open')
+  document.body.classList.toggle('overlay-open')
 }
 
 function intersectionHandler(entries) {
-  entries.forEach((entry) => {
-    console.log(entry);
+  entries.forEach(entry => {
     entry.target.classList.toggle(
-      "intersecting",
+      'intersecting',
       entry.intersectionRatio > 0 && entry.intersectionRatio < 1
-    );
-  });
+    )
+  })
 }
